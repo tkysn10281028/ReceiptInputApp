2c5ca1019b170fe2557a6d054107627318c716bb
tkysn1028 <sono@oysterworld.jp>
2022-08-13 11:31:29 +0900

    modified URI

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main(){
 	server := http.Server{
 		Addr: ":8082",
 	}
-	http.Handle("/", http.FileServer(http.Dir("./test")))
+	http.Handle("/receipt", http.FileServer(http.Dir("./test")))
+	fmt.Println("server listening...")
 	server.ListenAndServe()
 }
