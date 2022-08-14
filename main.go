f11091642c8392340825f9c1de83d78f6160014c
tkysn1028 <sono@oysterworld.jp>
2022-08-14 20:56:37 +0900

    commit for test

--- main.go
+++ main.go
@@ -12,5 +12,10 @@ func main(){
 	}
 	http.Handle("/", http.FileServer(http.Dir("./test")))
 	fmt.Println(time.Now().String() + "server listening...")
+	hello()
 	server.ListenAndServe()
+}
+
+func hello(){
+	fmt.Println("hello")
 }
